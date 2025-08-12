1c8af9f1d8f20367e802a49899ffbc9229f28f89
Ngassa-dev <ngassa.guendjio@2028.ucac-icam.com>
2025-08-12 13:49:42 +0100

    bien

--- frontend/src/DashboardAdmin.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function DashboardAdmin() {
-  return <h2>Bienvenue sur le Dashboard Admin !</h2>;
-}
--- frontend/src/DashboardClient.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function DashboardClient() {
-  return <h2>Bienvenue sur le Dashboard Client !</h2>;
-}
